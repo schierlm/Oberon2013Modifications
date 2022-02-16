053124d4e5b4ac2adfff0fdee769a3cf103ab01b
Michael Schierl <schierlm@gmx.de>
2022-02-16 23:22:14 +0100

    Clean up some quirks
    Not having a way to automatically run a command at startup yielded some
    creative solutions, which can be removed now.
    
    Also modify CommandLineDefragger to use a hook in the normal Defragger
    instead of a patched module. Makes it easier to remove
    CommandLineDefragger before finally defragmenting the disk images.
    
    Last but not least, fix broken Clipboard and PCLink in DebugWithRescue
    image's rescue mode.

--- make_disk_image.sh
+++ make_disk_image.sh
@@ -10,12 +10,9 @@ for i in MenuViewers Rectangles Curves Draw MacroTool PCLink1 ORTool; do
 done
 
 cd work
-sed 's/Modules.Load("System"/Modules.Load("CommandLineSystem"/' <Oberon.Mod.txt >OberonX.Mod.txt
-cp Defragger.Mod.txt Defragger0.Mod.txt
-patch <../CommandLineCompiler/CommandLineDefragger.patch
-mv Defragger.Mod.txt DefraggerX.Mod.txt
-mv Defragger0.Mod.txt Defragger.Mod.txt
+echo '@Startup: CommandLineSystem.Run' >System0.Tool.txt
 cp ../Clipboard.Mod.txt .
+cp ../CommandLineCompiler/Comm*.Mod.txt .
 for i in *.txt debug/*.txt rescue/*.txt debugrescue/*.txt; do unix2mac $i; mv $i ${i%.txt}; done
 
 unzip ../CommandLineCompiler.zip
@@ -28,34 +25,31 @@ for i in *.Mod *.Text *.Tool; do echo +$i >> .cmds; done
 head -n 4 ../BuildModifications.Tool.txt > .cmds
 ./risc mods.dsk < .cmds
 head -n -6 ../BuildModifications.Tool.txt | tail -n +6 > .cmds
-echo 'ORP.Compile CommandLineSystem.Mod OberonX.Mod PCLink1.Mod ~' >> .cmds
+echo 'ORP.Compile CommandLineSystem.Mod/s CommandLineDefragger.Mod PCLink1.Mod Clipboard.Mod ~' >> .cmds
+echo 'System.RenameFiles System.Tool => System1.Tool System0.Tool => System.Tool ~' >> .cmds
 ./risc mods.dsk < .cmds
 cp mods.dsk mods0.dsk
-echo 'ORP.Compile DefraggerX.Mod/s ~' > .cmds
-echo 'System.DeleteFiles OberonX.Mod DefraggerX.Mod ~' >> .cmds
-echo 'Defragger.Load' >> .cmds
-echo 'ORP.Compile Oberon.Mod Defragger.Mod/s ~' >> .cmds
-echo 'Defragger.Defrag' >> .cmds
+echo 'CommandLineDefragger.Load' > .cmds
+echo 'System.DeleteFiles CommandLineDefragger.Mod CommandLineDefragger.rsc CommandLineSystem.Mod CommandLineSystem.rsc ~' >> .cmds
+echo 'System.RenameFiles System1.Tool => System.Tool ~' >> .cmds
+echo 'CommandLineDefragger.Defrag' >> .cmds
 ./risc mods.dsk < .cmds
 ../DefragmentFreeSpace/trim_defragmented_image.sh mods.dsk
 mv mods.dsk_trimmed OberonModifications.dsk
 
 cd rescue
-echo 'ORP.Compile Oberon.Mod ~' > .cmds
-echo 'System.RenameFiles Oberon.rsc => Oberon.rsc.RS ~' >> .cmds
+echo 'System.RenameFiles Oberon.rsc => Oberon.rsc.RS ~' > .cmds
 for i in *; do echo +$i >> .cmds; done
-echo 'ORP.Compile RescueSystemTool.Mod/s RescueSystemLoader.Mod/s OberonX.Mod ~' >> .cmds
-echo 'ORP.Compile Kernel.Mod FileDir.Mod Files.Mod Modules.Mod ~' >> .cmds
+echo 'ORP.Compile RescueSystemTool.Mod/s RescueSystemLoader.Mod/s ~' >> .cmds
+echo 'ORP.Compile Kernel.Mod FileDir.Mod Files.Mod Modules.Mod Oberon.Mod ~' >> .cmds
 echo 'ORL.Link Modules ~' >> .cmds
 echo 'ORL.Load Modules.bin ~' >> .cmds
 echo 'RescueSystemTool.MoveFilesystem' >> .cmds
 ../risc ../mods0.dsk < .cmds
-sed 's/Modules.Load("System"/Modules.Load("CommandLineSystem"/' <Oberon.Mod >OberonX.Mod
-echo '+OberonX.Mod' > .cmds
-echo 'ORP.Compile Modules.RS.Mod Fonts.Embedded.Mod System.RS.Mod ~' >> .cmds
+echo 'ORP.Compile Modules.RS.Mod Fonts.Embedded.Mod System.RS.Mod ~' > .cmds
 echo 'ORL.Link Modules ~' >> .cmds
 echo 'System.RenameFiles Modules.bin => Modules.bin.RS Fonts.rsc => Fonts.rsc.RS System.rsc => System.rsc.RS ~' >> .cmds
-echo 'ORP.Compile Modules.Mod Fonts.Mod System.Mod OberonX.Mod ~' >> .cmds
+echo 'ORP.Compile Modules.Mod Fonts.Mod System.Mod ~' >> .cmds
 echo 'ORL.Link Modules ~' >> .cmds
 echo 'System.CopyFiles Input.rsc => Input.rsc.RS Display.rsc => Display.rsc.RS Viewers.rsc => Viewers.rsc.RS' \
      'Texts.rsc => Texts.rsc.RS MenuViewers.rsc => MenuViewers.rsc.RS TextFrames.rsc => TextFrames.rsc.RS' \
@@ -77,48 +71,44 @@ for i in *.Mod *.Text *.Tool; do echo +$i >> .cmds; done
 head -n 4 ../BuildModifications.Tool.txt > .cmds
 ./risc dbgmods.dsk < .cmds
 grep -v 'DEBUG VERSION ONLY' ../BuildModifications.Tool.txt | tail -n +6 | sed 's#/s#/s/d#g' > .cmds
-echo 'ORP.Compile CommandLineSystem.Mod/s/d OberonX.Mod PCLink1.Mod/s/d ~' >> .cmds
+echo 'ORP.Compile CommandLineSystem.Mod/s/d CommandLineDefragger.Mod/s PCLink1.Mod/s/d Clipboard.Mod/s/d ~' >> .cmds
+echo 'System.RenameFiles System.Tool => System1.Tool System0.Tool => System.Tool ~' >> .cmds
 ./risc dbgmods.dsk < .cmds
 cp dbgmods.dsk dbgmods0.dsk
-echo 'ORP.Compile DefraggerX.Mod/s ~' > .cmds
-echo 'System.DeleteFiles OberonX.Mod DefraggerX.Mod ~' >> .cmds
-echo 'Defragger.Load' >> .cmds
-echo 'ORP.Compile Oberon.Mod/s/d Defragger.Mod/s/d ~' >> .cmds
-echo 'Defragger.Defrag' >> .cmds
+echo 'CommandLineDefragger.Load' > .cmds
+echo 'System.DeleteFiles CommandLineDefragger.Mod CommandLineDefragger.rsc CommandLineSystem.Mod CommandLineSystem.rsc ~' >> .cmds
+echo 'System.RenameFiles System1.Tool => System.Tool ~' >> .cmds
+echo 'CommandLineDefragger.Defrag' >> .cmds
 ./risc dbgmods.dsk < .cmds
 ../DefragmentFreeSpace/trim_defragmented_image.sh dbgmods.dsk
 mv dbgmods.dsk_trimmed OberonModificationsDebug.dsk
 
-rm rescue/OberonX.Mod
 mv debugrescue/* rescue
 cd rescue
 echo 'System.RenameFiles Oberon.Mod => OberonY.Mod ~' > .cmds
 for i in *; do echo +$i >> .cmds; done
-echo 'ORP.Compile RescueSystemTool.Mod/s/d RescueSystemLoader.Mod/s/d OberonX.Mod/d ~' >> .cmds
+echo 'ORP.Compile RescueSystemTool.Mod/s/d RescueSystemLoader.Mod/s/d ~' >> .cmds
 echo 'ORP.Compile Kernel.Mod/d FileDir.Mod/d Files.Mod/d Modules.Mod/d ~' >> .cmds
 echo 'ORL.Link Modules ~' >> .cmds
 echo 'ORL.Load Modules.bin ~' >> .cmds
 echo 'RescueSystemTool.MoveFilesystem' >> .cmds
 ../risc ../dbgmods0.dsk < .cmds
-sed 's/Modules.Load("System"/Modules.Load("CommandLineSystem"/' <Oberon.Mod >OberonX.Mod
-echo '+OberonX.Mod' > .cmds
-echo 'ORP.Compile Modules.RS.Mod Fonts.Embedded.Mod/s Texts.Mod/s OberonY.Mod/s ~' >> .cmds
+echo 'ORP.Compile Modules.RS.Mod Fonts.Embedded.Mod/s Texts.Mod/s OberonY.Mod/s ~' > .cmds
 echo 'ORL.Link Modules ~' >> .cmds
-echo 'ORP.Compile MenuViewers.Mod/s TextFrames.Mod/s System.RS.Mod/s Edit.Mod/s ORL.Mod/s ~' >> .cmds
+echo 'ORP.Compile MenuViewers.Mod/s TextFrames.Mod/s System.RS.Mod/s Edit.Mod/s Clipboard.Mod/s PCLink1.Mod/s ORL.Mod/s ~' >> .cmds
 echo 'ORP.Compile ORS.Mod/s ORB.Mod/s ~' >> .cmds
 echo 'ORP.Compile ORG.Mod/s ORP.Mod/s ~' >> .cmds
 echo 'System.RenameFiles Modules.bin => Modules.bin.RS Fonts.rsc => Fonts.rsc.RS' \
      'Texts.rsc => Texts.rsc.RS MenuViewers.rsc => MenuViewers.rsc.RS' \
-     'TextFrames.rsc => TextFrames.rsc.RS Edit.rsc => Edit.rsc.RS' \
-     'System.rsc => System.rsc.RS Oberon.rsc => Oberon.rsc.RS ORL.rsc => ORL.rsc.RS' \
+     'TextFrames.rsc => TextFrames.rsc.RS Edit.rsc => Edit.rsc.RS Clipboard.rsc => Clipboard.rsc.RS' \
+     'System.rsc => System.rsc.RS Oberon.rsc => Oberon.rsc.RS ORL.rsc => ORL.rsc.RS PCLink1.rsc => PCLink1.rsc.RS' \
      'ORS.rsc => ORS.rsc.RS ORB.rsc => ORB.rsc.RS ORG.rsc => ORG.rsc.RS ORP.rsc => ORP.rsc.RS ~' >> .cmds
-echo 'ORP.Compile Modules.Mod/d Fonts.Mod/s/d Texts.Mod/s/d OberonX.Mod/s/d ~' >> .cmds
-echo 'ORP.Compile MenuViewers.Mod/s/d TextFrames.Mod/s/d System.Mod/s/d Edit.Mod/s/d ORL.Mod/s/d ~' >> .cmds
+echo 'ORP.Compile Modules.Mod/d Fonts.Mod/s/d Texts.Mod/s/d Oberon.Mod/s/d ~' >> .cmds
+echo 'ORP.Compile MenuViewers.Mod/s/d TextFrames.Mod/s/d System.Mod/s/d Edit.Mod/s/d Clipboard.Mod/s/d PCLink1.Mod/s/d ORL.Mod/s/d ~' >> .cmds
 echo 'ORP.Compile ORS.Mod/s/d ORB.Mod/s/d ~' >> .cmds
 echo 'ORP.Compile ORG.Mod/s/d ORP.Mod/s/d ~' >> .cmds
 echo 'ORL.Link Modules ~' >> .cmds
-echo 'System.CopyFiles Input.rsc => Input.rsc.RS Display.rsc => Display.rsc.RS Viewers.rsc => Viewers.rsc.RS' \
-     'PCLink1.rsc => PCLink1.rsc.RS Clipboard.rsc => Clipboard.rsc.RS'  >> .cmds
+echo 'System.CopyFiles Input.rsc => Input.rsc.RS Display.rsc => Display.rsc.RS Viewers.rsc => Viewers.rsc.RS ~'  >> .cmds
 echo 'RescueSystemTool.LoadRescue' >> .cmds
 echo 'System.DeleteFiles Fonts.Embedded.Mod OberonY.Mod Modules.RS.Mod System.RS.Mod ~' >> .cmds
 ../risc ../dbgmods0.dsk < .cmds
