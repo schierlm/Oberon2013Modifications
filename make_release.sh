a9589cef025b31b30448cf5d9105e31e23efe75d
Markow Eduard <ulrichulrich_se@yahoo.com>
2022-02-18 20:32:09 +0100

    make_release.sh improved, EditU.0.patch fix

--- make_release.sh
+++ make_release.sh
@@ -48,8 +48,8 @@ patch -d work <FontConversion/RemoveGlyphWidthLimit.patch
 patch -d work <ChangeResolution/ChangeResolution.patch
 patch -d work <LanguageServerProtocolHelper/LSPHelper.patch
 patch -d work <KernelDebugger/RS232.patch
-patch -d work <EditImprovements/EditU.0.patch
 patch -d work <EditImprovements/Edit.1r.patch
+patch -d work <EditImprovements/EditU.0.patch
 
 mkdir work/utf8lite
 cp work/Fonts.Mod.txt work/TextFrames.Mod.txt work/utf8lite
