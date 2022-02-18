c6fba5e4a440b974aedc7d54aa6f1a4632a48eec
Markow Eduard <ulrichulrich_se@yahoo.com>
2022-02-18 20:10:01 +0100

    EditU.0.patch, make_release.sh

--- make_release.sh
+++ make_release.sh
@@ -49,6 +49,7 @@ patch -d work <ChangeResolution/ChangeResolution.patch
 patch -d work <LanguageServerProtocolHelper/LSPHelper.patch
 patch -d work <KernelDebugger/RS232.patch
 patch -d work <EditImprovements/Edit.1r.patch
+patch -d work <EditImprovements/EditU.0.patch
 
 mkdir work/utf8lite
 cp work/Fonts.Mod.txt work/TextFrames.Mod.txt work/utf8lite
